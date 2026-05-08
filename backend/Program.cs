20b4f82c596aec70c8e1a5d5dfe443a71cea9a07
barbralopes <blcoelho51gmail.com>
2026-05-08 10:09:19 -0300

    ajustando conexão com o banco

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,6 @@
+using backend.Configurations;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -5,6 +8,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<MongoDbSettings>(
+    builder.Configuration.GetSection("MongoDbSettings"));
+
+builder.Services.AddSingleton<IMongoClient>(sp =>
+{
+    var settings = builder.Configuration
+        .GetSection("MongoDbSettings")
+        .Get<MongoDbSettings>();
+
+    return new MongoClient(settings!.ConnectionString);
+});
 
 var app = builder.Build();
 
--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6795715e62dbe115d9efdd1143bfef0d3d9f16bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c02e39999cda0f46545060d6dd8b0da0eb33f1ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
